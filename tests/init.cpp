a4bc3425fbcf95ceac1e6697d58af73cda6d93d8
NeverMore27 <uragan.uragan.59@mail.ru>
2017-03-21 01:22:34 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -22,3 +22,44 @@ SCENARIO("copy")
 	REQUIRE(a.re() == b.re());
 	
 }
+
+SCENARIO("+")
+{
+	complex a (4, 4), b(a);
+	complex c(8, 8);
+	b+=a;
+	REQUIRE(b== c);
+}
+
+SCENARIO("*") 
+{
+	complex b(3, 3), a(1,1), c(0,6);
+	b*=a;
+	REQUIRE(b==c);
+}
+
+SCENARIO("/") 
+{
+	complex b(3, 3), a(9,9), c(3, 0);
+	a/=b;
+	REQUIRE(b==c);
+}
+
+SCENARIO("==") 
+{
+	complex a(3, 3);
+	complexb (a);
+	REQUIRE(a == b);
+}
+
+SCENARIO("=") {
+	complex a(2, 2);
+	complex f = a;
+	REQUIRE(f == a);
+}
+
+SCENARIO("-") {
+	complex a(9, 9), b(3, 3), f(6, 6);
+	a-=b;
+	REQUIRE(f == a);
+}
