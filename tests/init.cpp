4131f77cb23ca9eaed1e37bd7f63634408c6533c
NeverMore27 <uragan.uragan.59@mail.ru>
2017-03-21 01:16:39 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,18 +8,3 @@ SCENARIO("init")
 	REQUIRE(a.re() == 0);
 }
 
-SCENARIO("param") 
-{
-	complex a(2, 2);
-	REQUIRE(a.im() == 2);
-	REQUIRE(a.re() == 2);
-}
-
-SCENARIO("copy")
-{
-	complex a (4, 4), b(a);	
-	REQUIRE(a.im() ==b.im());
-	REQUIRE(a.real() == b.real());
-	
-}
-
